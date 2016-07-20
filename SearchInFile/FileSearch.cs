33842a578f3b18a115022d84400ea939daf3fa6d
ili <ili@WS-ILI1UA1>
2016-07-20 17:37:36 +0300

    Fix

---
+++ SearchInFile/FileSearch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SearchInFile
+{
+    public class FileSearch
+    {
+       public static string Search(string str)
+        {
+            string fileContent = File.ReadAllText("D:\\Study\\CsharpTasks\\Task8\\SearchInFile\\SearchInFile\\bin\\Debug\\TextFile.txt");
+            if (fileContent.Contains(str))
+            {
+                return "The "+str+" is founded in file TextFile.txt";
+            }
+            else
+            {
+                return String.Empty;
+            }
+        }
+    }
+}
